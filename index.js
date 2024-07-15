a8630c24eab60ffbd7a2ae54fde6697a8b40860f
Isaiah <zaymgmt707@gmail.com>
2024-07-15 13:56:05 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Function that takes a callback function as an argument and invokes it
 const receivesAFunction = callback => callback()
+// Function that returns a named function
 const returnsANamedFunction = () => function namedFunction(){}
+// Function that returns an anonymous function
 const returnsAnAnonymousFunction = () => function(){}
